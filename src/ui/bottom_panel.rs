8771474de962f621b704b5d084cf1c40ff184e55
Kamil Democko <kamil.democko@gmail.com>
2025-03-09 09:36:10 +0100

    added version

--- src/ui/bottom_panel.rs
+++ src/ui/bottom_panel.rs
@@ -4,24 +4,30 @@ use eframe::egui::{Align, Layout, TextEdit};
 use crate::config;
 use crate::status::Status;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 
 pub fn render_bottom_panel(ui: &mut Ui, status: &mut Status, delete_checked: &mut bool) {
     let mut status_text = status.get_string();
 
-    ui.add_space(5.0);
+    ui.with_layout(Layout::right_to_left(Align::BOTTOM), |ui| {
+        ui.add_space(15.0);
+        ui.label(format!("v{}", VERSION));
+    });
+
     ui.add_sized(
-        [ui.available_width() - 20.0, 80.0], 
+        [ui.available_width() - 30.0, 80.0], 
         TextEdit::multiline(&mut status_text)
             .desired_rows(5)
             .interactive(false)
     );
 
     ui.horizontal(|ui| {
-        ui.add_space(10.0);
+        ui.add_space(15.0);
         ui.checkbox(delete_checked, "delete map after import");
 
         ui.with_layout(Layout::right_to_left(Align::BOTTOM), |ui| {
-            ui.add_space(10.0);
+            ui.add_space(15.0);
 
             if ui.link("Github").clicked() {
                 if let Err(err) = open::that(config::GITHUB_LINK) {
